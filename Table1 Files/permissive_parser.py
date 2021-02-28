96b7f28249741ace65d41e536a8cb66cc19319ff
Shenghui Chen <sc9by@virginia.edu>
2021-02-28 08:40:11 -0500

    updated permissive parser and result for reachable y_s,a

--- Table1 Files/permissive_parser.py
+++ Table1 Files/permissive_parser.py
@@ -11,17 +11,39 @@ for s in states:
         filenames.append(solution + '_' + str(s) + '_' + str(i+1) + '.sol')
 filenames.remove('out_combined_792_1.sol')
 
+# filenames = ['out_combined_22_1.sol']
+
 # create result file
 result = open('permissive_result',"w+")
-
 for filename in filenames:
     file = open(filename, 'r')
     counter = 0
+    reachable_states = {}
+
+    s_a_ct = 0
 
     for line in file:
         y_label = line.split()[0]
         if y_label[0] == 'y': # count the number of enabled y_s,a
-            y_value = line.split()[1]
-            counter += int(y_value)
-    print(counter)
+            s = y_label.split('_')[1]
+            y_value = int(line.split()[1])
+
+            if s not in reachable_states:
+                reachable_states[s] = [y_value]
+            else:
+                reachable_states[s].append(y_value)
+
+            counter += y_value
+    print("sum of y", counter)
     result.write(filename + ': ' + str(counter) + '\n')
+
+
+    total_y_num = 0
+    for s in reachable_states:
+        ys = reachable_states[s]
+        # print(ys)
+        if sum(ys) > 0:
+            total_y_num += len(ys)
+
+    print("total reachable y num", total_y_num)
+    result.write('total y num for ' + filename + ': ' + str(total_y_num) + '\n')
--- Table2 Files/permissive_parser.py
+++ Table2 Files/permissive_parser.py
@@ -12,15 +12,35 @@ for s in states:
 
 # create result file
 result = open('permissive_result',"w+")
-
 for filename in filenames:
     file = open(filename, 'r')
     counter = 0
+    reachable_states = {}
+
+    s_a_ct = 0
 
     for line in file:
         y_label = line.split()[0]
         if y_label[0] == 'y': # count the number of enabled y_s,a
-            y_value = line.split()[1]
-            counter += int(y_value)
-    print(counter)
+            s = y_label.split('_')[1]
+            y_value = int(line.split()[1])
+
+            if s not in reachable_states:
+                reachable_states[s] = [y_value]
+            else:
+                reachable_states[s].append(y_value)
+
+            counter += y_value
+    print("sum of y", counter)
     result.write(filename + ': ' + str(counter) + '\n')
+
+
+    total_y_num = 0
+    for s in reachable_states:
+        ys = reachable_states[s]
+        # print(ys)
+        if sum(ys) > 0:
+            total_y_num += len(ys)
+
+    print("total reachable y num", total_y_num)
+    result.write('total y num for ' + filename + ': ' + str(total_y_num) + '\n')
